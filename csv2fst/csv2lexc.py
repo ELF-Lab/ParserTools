594897b700a95e12ebc25118f4fe53a57ab8364f
Miikka Silfverberg <msilfver@mail.ubc.ca>
2024-02-05 18:18:06 -0800

    Path name fix

--- csv2fst/csv2lexc.py
+++ csv2fst/csv2lexc.py
@@ -15,7 +15,7 @@ def main(config_file,lexc_path,read_lexical_database):
     print(json.dumps(config, indent=2), file=stderr)
 
     # We'll first compile regular paradigms into a LEXC file 
-    print(f"Reading spreadsheets from directory: {config['path']}", file=stderr)
+    print(f"Reading spreadsheets from directory: {config['source_path']}", file=stderr)
     lexicon = Lexicon(config, lexc_path, read_lexical_database, regular=True)
     print(f"Writing lexc output to {config['regular_lexc_file']}", file=stderr)
     lexicon.print_lexc()
@@ -23,7 +23,7 @@ def main(config_file,lexc_path,read_lexical_database):
     # We'll then compile irregular paradigms into a different LEXC
     # file. These need to be separated because, later on, phonological
     # rules are only applied to regular paradigms.
-    print(f"Reading spreadsheets for irregular paradigms from directory: {config['path']}", file=stderr)
+    print(f"Reading spreadsheets for irregular paradigms from directory: {config['source_path']}", file=stderr)
     irregular_lexicon = Lexicon(config, lexc_path, read_lexical_database=False, regular=False)
     print(f"Writing lexc output to {config['irregular_lexc_file']}", file=stderr)
     irregular_lexicon.print_lexc()
