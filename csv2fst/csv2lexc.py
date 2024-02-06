cb1277b0f8c180e84fb1384491bf5958935f8eb7
Miikka Silfverberg <msilfver@mail.ubc.ca>
2024-02-05 17:07:00 -0800

    Add support for external lexical database and allow for running tests without adding exntries from the database.

--- csv2fst/csv2lexc.py
+++ csv2fst/csv2lexc.py
@@ -5,15 +5,18 @@ import json
 from lexicon import Lexicon
 
 @click.command()
-@click.option('--config-file', required=True, help='JSON config file')
-def main(config_file):
+@click.option('--config-file', required=True, help="JSON config file")
+@click.option('--lexc-path', required=True, help="Directory where output lexc files are stored")
+@click.option('--read-lexical-database', required=False, default=True,
+              help="Whether to include lexemes from an external lexicon database")
+def main(config_file,lexc_path,read_lexical_database):
     print(f"Configure file {config_file}:", file=stderr)
     config = json.load(open(config_file))
     print(json.dumps(config, indent=2), file=stderr)
 
     # We'll first compile regular paradigms into a LEXC file 
     print(f"Reading spreadsheets from directory: {config['path']}", file=stderr)
-    lexicon = Lexicon(config, regular=True)
+    lexicon = Lexicon(config, lexc_path, read_lexical_database, regular=True)
     print(f"Writing lexc output to {config['regular_lexc_file']}", file=stderr)
     lexicon.print_lexc()
 
@@ -21,7 +24,7 @@ def main(config_file):
     # file. These need to be separated because, later on, phonological
     # rules are only applied to regular paradigms.
     print(f"Reading spreadsheets for irregular paradigms from directory: {config['path']}", file=stderr)
-    irregular_lexicon = Lexicon(config, regular=False)
+    irregular_lexicon = Lexicon(config, lexc_path, read_lexical_database=False, regular=False)
     print(f"Writing lexc output to {config['irregular_lexc_file']}", file=stderr)
     irregular_lexicon.print_lexc()
     
--- csv2fst/lexicon.py
+++ csv2fst/lexicon.py
@@ -1,12 +1,25 @@
-from paradigm_slot import ParadigmSlot, entry2str
+from paradigm_slot import ParadigmSlot, entry2str, LexcEntry
 import json
 import pandas as pd
 from sys import stderr
 import os
+import re
 
 class Lexicon:
-    def __init__(self, conf:dict, regular:bool):
+    @staticmethod
+    def __get_paradigm(row,klass_map):
+        for _, pattern in klass_map.iterrows():
+            if row["part_of_speech_id"].lower() != pattern["OPDClass"].lower():
+                continue
+            element = (row["lemma"] if pattern["MatchElement"] == "lemma"
+                                    else row["stem"])
+            if re.match(pattern["Pattern"], element):
+                return pattern["Class"]
+        raise ValueError(f"No matching pattern for lexical entry: {row}")
+            
+    def __init__(self, conf:dict, lexc_path:str, read_lexical_database:bool, regular:bool):
         self.conf = conf
+        self.lexc_path = lexc_path
         self.regular = regular
         self.lexicons = {"Root":set()}
         for fn in conf["regular_csv_files" if regular else "irregular_csv_files"]:
@@ -16,10 +29,37 @@ class Lexicon:
             for _, row in table.iterrows():
                 paradigm_slot = ParadigmSlot(row, conf, regular)
                 paradigm_slot.extend_lexicons(self.lexicons)
+        if read_lexical_database:
+            self.read_lexemes_from_database()
             
+    def read_lexemes_from_database(self):
+        print(f"Reading external lexical database {self.conf['lexical_database']}", file=stderr)
+        print(f"Reading class mapping {self.conf['class_map']}", file=stderr)
+        klass_map = pd.read_csv(self.conf["class_map"])
+        lexeme_database = pd.read_csv(self.conf["lexical_database"], keep_default_na=False)
+        skipped = 0
+        for _, row in lexeme_database.iterrows():
+            try:
+                klass = Lexicon.__get_paradigm(row,klass_map)
+                paradigm = klass.split("_")[0]
+                self.lexicons[f"{paradigm}:Stems"].add(
+                    LexcEntry(f"{paradigm}:Stems",
+                              row["lemma"],
+                              row["stem"],
+                              f"{paradigm}:Class={klass}:Boundary"))
+            except ValueError as e:
+                print(e)
+                print("Skipping lexical entry which does not match any patterns\n")
+                skipped += 1
+        print(f"Checked {len(lexeme_database)} lexical entries.", file=stderr)
+        print(f"Added {len(lexeme_database) - skipped} entries to lexc file.", file=stderr) 
+        print(f"Skipped {skipped} invalid ones", file=stderr)
+
     def print_lexc(self):
-        lexc_file = open(self.conf["regular_lexc_file" if self.regular
-                                   else "irregular_lexc_file"],"w")
+        lexc_fn = os.path.join(self.lexc_path,
+                               self.conf["regular_lexc_file" if self.regular
+                                         else "irregular_lexc_file"])
+        lexc_file = open(lexc_fn,"w")
         print("Multichar_Symbols",file=lexc_file)
         multichar_symbols = sorted(list(ParadigmSlot.multichar_symbols))
         print(" ".join(multichar_symbols), file=lexc_file)
