a5c129f8efa67b0faf076ca1cfcf42577b84be7a
Anna <annajs@student.ubc.ca>
2024-06-20 21:32:36 -0700

    Added count of number of forms with no results (i.e., "Unexpected results: +?") to test summary output.

--- csv2fst/test_summary.py
+++ csv2fst/test_summary.py
@@ -12,7 +12,7 @@ PRINT_DETAILED_EVAL = False
 TEST_SECTIONS = ["VAIO", "VAI_V", "VAI_VV", "VAI_am", "VAI_n", "VII_V", "VII_VV", "VII_d", "VII_n", "VTA_C", "VTA_Cw", "VTA_aw", "VTA_n", "VTI_aa", "VTI_am", "VTI_i", "VTI_oo"]
 
 def write_to_csv(output_line):
-    HEADER = "Date," + ",".join(TEST_SECTIONS) +  ",Total passes,Total passes (%)"
+    HEADER = "Date," + ",".join(TEST_SECTIONS) +  ",Total forms with no results, Total passes,Total passes (%)"
     if not os.path.isfile(OUTPUT_FILE_NAME):
             with open(OUTPUT_FILE_NAME, "w+") as csv_file:
                 print(HEADER, file = csv_file)
@@ -36,6 +36,7 @@ def prepare_output(results):
     output_line = str(date.today()) + "," # First column is the date!
     total_passes = 0
     total_tests = 0
+    total_forms_with_no_results = 0
     for test_section in TEST_SECTIONS:
         if ADD_APOSTROPHE:
             output_line += "'"
@@ -46,11 +47,14 @@ def prepare_output(results):
             # Add the results from this test section to our output line
             output_line += results[test_section]["log_passes"] + "/" + results[test_section]["log_total"]
             if PRINT_DETAILED_EVAL:
-                output_line += " False pos: " + str(results[test_section]["false_pos"]) + " False neg: " + str(results[test_section]["false_neg"])
+                output_line += " False pos: " + str(results[test_section]["false_pos"]) + " False neg: " + str(results[test_section]["false_neg"])+ " Forms with no results: " + str(results[test_section]["forms_with_no_results"])
             output_line += ","
             total_passes += int(results[test_section]["log_passes"])
             total_tests += int(results[test_section]["log_total"])
+            total_forms_with_no_results += results[test_section]["forms_with_no_results"]
     
+    # Some summary info
+    output_line += str(total_forms_with_no_results) + ","
     # Last columns are the total count and then percent
     output_line += str(total_passes) + "/" + str(total_tests) + ","
     output_line += "{:.0%}".format(total_passes / total_tests)
@@ -69,18 +73,21 @@ def read_logs():
             test_section = test_section.replace(".yaml", "")
             false_pos = 0
             false_neg = 0
+            forms_with_no_results = 0
         elif "Missing results" in line:
             false_neg += 1
-        elif search("Unexpected results: [a-z]", line):
+        elif search(r"Unexpected results: [a-z]", line):
             false_pos += 1
+        elif search(r"Unexpected results: \+\?", line):
+            forms_with_no_results += 1
         # Get the # of fails / # of tests
         elif line.startswith("Total"):
             section_results = line.strip()
             section_results = section_results.replace("Total passes: ", "")
-            section_results = split(", Total fails: [0-9]+, Total: ", section_results)
+            section_results = split(r", Total fails: [0-9]+, Total: ", section_results)
             log_passes = section_results[0]
             log_total = section_results[1]
-            test_section_results = {"log_passes": log_passes, "log_total": log_total, "false_pos": false_pos, "false_neg": false_neg}
+            test_section_results = {"log_passes": log_passes, "log_total": log_total, "false_pos": false_pos, "false_neg": false_neg, "forms_with_no_results": forms_with_no_results}
             results.update({test_section: test_section_results})
 
     assert len(results) > 0, "\nERROR: The log file didn't have any test results to read!"
