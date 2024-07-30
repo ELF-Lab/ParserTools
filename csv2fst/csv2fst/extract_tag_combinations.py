5a44aea5fae19d15d1d9670642d99bc6c0ec7371
Miikka Silfverberg <msilfver@mail.ubc.ca>
2024-07-30 12:21:14 -0700

    Extract tags from all POS

--- csv2fst/csv2fst/extract_tag_combinations.py
+++ csv2fst/csv2fst/extract_tag_combinations.py
@@ -14,15 +14,28 @@ def get_tags(tag_list):
 @click.command()
 @click.option('--config-file', required=True, help="JSON config file. E.g. verb_conf.json")
 @click.option('--source-path', required=True, help="Path to the source files for the FST. E.g. your BorderLakesMorph directory")
-@click.option('--pre-element', required=True, help="Name of pre-element slot. E.g. TensePreverbs")
+@click.option('--pre-element', required=False, default=None, help="Name of pre-element slot. E.g. TensePreverbs")
 @click.option('--pre-element-tags', required=False, help="Possible pre-element (preverb/prenoun) tags which can occur before the stem. E.g. \"PVTense/gii,PVTense/wii,PVTense/wii',0\"")
+@click.option('--post-element', required=False, default=None, help="Name of post-element slot. E.g. Derivation")
+@click.option('--post-element-tags', required=False, help="Possible post-element (augment/derivation) tags which can occur right after the stem E.g. \"VAI+Der/magad,VII+Aug/magad,VTA+Reflex/dizo,VTA+Reflex/di,0\"")
 @click.option('--output-file', required=True, help="Output json file name")
-def main(config_file, source_path, pre_element, pre_element_tags, output_file):
+def main(config_file, source_path, pre_element, pre_element_tags, post_element, post_element_tags, output_file):
     info(f"Processing configuration file {config_file}")
     config = json.load(open(config_file))
     tags = {}
-    tags["access_order"] = [pre_element, "Stem"] + config["morph_features"]
-    tags[pre_element] = pre_element_tags.split(",")
+    
+    # Set access order
+    tags["access_order"] = []
+    if pre_element:
+        tags["access_order"] = [pre_element] + ["Stem"]
+        tags[pre_element] = pre_element_tags.split(",")
+    else:
+        tags["access_order"] = ["Stem"]
+    if post_element:
+        tags["access_order"] += [post_element]
+        tags[post_element] = post_element_tags.split(",")
+    tags["access_order"] += config["morph_features"]
+
     for feature in config["morph_features"]:
         tags[feature] = set()        
     csv_files = config["regular_csv_files"] + config["irregular_csv_files"]
