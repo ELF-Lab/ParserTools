e5003c7de93414cac5d16bd967912a825f7e24f0
Anna <annajs@student.ubc.ca>
2025-04-25 17:35:41 -0500

    Renamed csv2fst/csv2fst/ to csv2fst/src. 	- Also deleted a .sh that was accidentally committed. 	- Also removed a .gitignore since there's already one in the root.

--- csv2fst/testy.sh
+++
@@ -1,1 +0,0 @@
-python3 /Users/Anna/Documents/other/cs_stuff/giella-core/scripts/morph-test.py --app flookup --surface --mor generated/ojibwe.noAlt.fomabin database_yaml_output/VII_V.yaml > opd-test-short.log
